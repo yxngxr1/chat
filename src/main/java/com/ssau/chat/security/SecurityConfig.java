032a342a705817e53297936b2476164ee6519f83
yxngxr1 <gderganov@yandex.ru>
2025-03-31 15:50:50 +0400

    changed cors

--- src/main/java/com/ssau/chat/security/SecurityConfig.java
+++ src/main/java/com/ssau/chat/security/SecurityConfig.java
@@ -75,7 +75,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+        configuration.setAllowedOriginPatterns(Arrays.asList("*"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
